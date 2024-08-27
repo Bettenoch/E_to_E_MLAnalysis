eedf47823609e94e5ebad9a59d2cec4fbbbe8387
Bettenoch <bettenoch254@gmail.com>
2024-08-27 10:31:26 +0300

    Remove the appended class.

--- strat_split_data.py
+++ strat_split_data.py
@@ -8,10 +8,12 @@ def stratified_split_data(housing, test_size=0.2, random_state=42):
     splitter = StratifiedShuffleSplit(n_splits=10, test_size=test_size, random_state=random_state,)
     strat_splits = []
     for train_index, test_index in splitter.split(housing, housing["income_cat"]):
-        strat_train_set_n = housing.iloc[train_index]
-        strat_test_set_n = housing.iloc[test_index]
+        strat_train_set_n = housing.iloc[train_index].copy()
+        strat_test_set_n = housing.iloc[test_index].copy()
         strat_splits.append([strat_train_set_n, strat_test_set_n])
         strat_train_set, strat_test_set = strat_splits[0]
+    for set_ in (strat_train_set, strat_test_set):
+        set_.drop("income_cat", axis=1, inplace=True)
         
         return strat_train_set, strat_test_set
             
